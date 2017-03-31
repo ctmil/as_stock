5db49d095073ac5c2cef98a4c195acfe02e0e637
morrillo <gustavo.orrillo@gmail.com>
2017-03-31 15:33:12 -0300

    Added as.stock.move

--- models_old.py
+++ models_old.py
@@ -34,4 +34,35 @@ class as_stock_quant(osv.osv):
 				select max(a.id) as id,a.location_id as location_id,a.company_id as company_id,
 					a.product_id as product_id,sum(qty) as qty from stock_quant a group by 2,3,4
 	        	""")
-                                                 
+
+
+class as_stock_move(osv.osv):
+	_name = "as.stock.move"
+	_description = "AS Stock Move"
+	_auto = False
+
+	_columns = {
+		'name': fields.char('Name'),
+		'date': fields.date('Date'),
+		'company_id': fields.many2one('res.company','Company'),
+		'product_id': fields.many2one('product.product','Product'),
+		'location_id': fields.many2one('stock.location','Location'),
+		'location_usage': fields.char('Location Usage'),
+		'location_dest_id': fields.many2one('stock.location','Location'),
+		'location_dest_usage': fields.char('Location Usage'),
+		'product_uom_qty': fields.integer('Qty'),
+		'cantidad': fields.integer('Qty'),
+		'loc_id': fields.many2one('stock.location','Location'),
+		}
+
+	def init(self, cr):
+        	tools.sql.drop_view_if_exists(cr, 'as_stock_move')
+	        cr.execute("""
+			create view as_stock_move as 
+				select a.id as id,a.name,a.date,a.company_id as company_id,a.product_id as product_id,a.location_id as location_id,b.usage as location_usage,
+				a.location_dest_id as location_dest_id,c.usage as location_dest_usage,a.product_uom_qty as product_uom_qty,
+				case b.usage when 'internal' then a.product_uom_qty * (-1) else a.product_uom_qty end as cantidad, 
+				case b.usage when 'internal' then a.location_id else a.location_dest_id end as loc_id 
+				from stock_move a inner join stock_location b on a.location_id = b.id inner join stock_location c on c.id = a.location_dest_id 
+				where (b.usage = 'internal' or c.usage = 'internal') and a.state = 'done'
+	        	""")
